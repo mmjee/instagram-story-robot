dc964d44a16336d609c55260dbb6f9b0d6d3de46
Maharshi Mukherjee <29047033+mmjee@users.noreply.github.com>
2021-03-06 21:08:41 +0530

    Update main.js

--- isr/main.js
+++ isr/main.js
@@ -1,6 +1,6 @@
 /*
     An Instagram story scraping robot
-    Copyright (C) 2021  the1337guy
+    Copyright (C) 2021  Maharshi Mukherjee
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
